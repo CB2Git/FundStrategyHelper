918921fd68db45d07141b01da44c9d812eab6973
chenbing91 <chenbing90@jd.com>
2020-04-30 16:47:10 +0800

    首次提交

---
+++ app/src/main/java/com/fund/strategy/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fund.strategy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
